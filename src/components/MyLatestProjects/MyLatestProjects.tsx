d3643df7c3aa8338e565960499bb23ec9890d9ee
irvinglara <irvinglara9115@gmail.com>
2024-08-24 12:58:23 -0600

    ajustes del color

--- src/components/MyLatestProjects/MyLatestProjects.tsx
+++ src/components/MyLatestProjects/MyLatestProjects.tsx
@@ -2,26 +2,27 @@ import React from "react";
 import {Box, Button, styled} from "@mui/material";
 import {useTranslation} from "react-i18next";
 import Typography, {TypographyProps} from '@mui/material/Typography';
-import {i} from "vite/dist/node/types.d-aGj9QkWt";
 
 
-interface CustomizedTypographyGradientProps extends Omit<TypographyProps, 'color'> { // Omitir props conflictivas
+interface CustomizedTypographyGradientProps extends TypographyProps {
     text?: string;
 }
 
-// @ts-ignore
-const CustomizedTypographyGradient = styled(({text = "hola", ...props}: CustomizedTypographyGradientProps) => (
+const CustomizedTypographyGradient = styled(({ text = 'Hola', component = 'span', ...props }: CustomizedTypographyGradientProps) => (
     <Typography
+        component={component}
         {...props}
-    >{text}</Typography>
+    >
+        {text}
+    </Typography>
 ))`
-    background: linear-gradient(90deg, #0ea5ea, #0bd1d1 51%, #0ea5ea) var(--x, 0)/200%;
-    font-weight: 700;
-    -webkit-background-clip: text !important;
-    -webkit-text-fill-color: transparent !important;
-    background-clip: text !important;
-    text-fill-color: transparent
-` as typeof Typography;
+  background: linear-gradient(90deg, #0ea5ea, #0bd1d1 51%, #0ea5ea) 0/200%;
+  font-weight: 700;
+  -webkit-background-clip: text !important;
+  -webkit-text-fill-color: transparent !important;
+  background-clip: text !important;
+  text-fill-color: transparent;
+`;
 
 const list = ["All Projects", "Web Developer", "Mobile App"]
 const MyLatestProjects = () => {
@@ -29,8 +30,10 @@ const MyLatestProjects = () => {
 
     return (<Box sx={{display: 'flex', flexDirection: 'column', width: '100%', height: '100%',gap:5}}>
         <Box sx={{display: 'flex', flexDirection: 'column', width: '100%', alignItems: 'center'}}>
-            <CustomizedTypographyGradient text={t('myProjects')} fontFamily={"Roboto"} fontSize={{xs: 30, md: 40}}
-                                          />
+            {/*<CustomizedTypographyGradient text={t('myProjects')} fontFamily={"Roboto"} fontSize={{xs: 30, md: 40}}
+                                          component="span"
+                                          />*/}
+            <CustomizedTypographyGradient variant="h4"  text={t('myProjects')}/>
         </Box>
         <Box
             sx={{
