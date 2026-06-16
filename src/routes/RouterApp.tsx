b3a5c9acf70745337d2e6d140e50b6a90cda1d87
irvinglara <irvinglara9115@gmail.com>
2026-06-16 16:13:54 -0600

    nuevo diseño ya justes

--- src/routes/RouterApp.tsx
+++ src/routes/RouterApp.tsx
@@ -4,7 +4,6 @@ import NotFoundPage from "../pages/NotFoundPage/NotFoundPage.tsx";
 import LandingPage from "../pages/LandingPage/LandingPage.tsx";
 import AboutMePage from "../pages/AboutMe/AboutMe.tsx";
 import Header from "../components/Header/Header.tsx";
-import Footer from "../components/Footer.tsx";
 
 const RouterApp: React.FC = () => {
 
