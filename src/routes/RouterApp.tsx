051b471565b5007cc1b47f90bc4fd23e829a21b1
irvinglara <irvinglara9115@gmail.com>
2026-06-16 16:36:47 -0600

    nuevo diseño ya justes

--- src/routes/RouterApp.tsx
+++ src/routes/RouterApp.tsx
@@ -4,11 +4,13 @@ import NotFoundPage from "../pages/NotFoundPage/NotFoundPage.tsx";
 import LandingPage from "../pages/LandingPage/LandingPage.tsx";
 import AboutMePage from "../pages/AboutMe/AboutMe.tsx";
 import Header from "../components/Header/Header.tsx";
+import ScrollToTop from "../components/ScrollToTop/ScrollToTop.tsx";
 
 const RouterApp: React.FC = () => {
 
     return (
         <BrowserRouter>
+            <ScrollToTop />
             <Header/>
             <Routes>
                 <Route path="/" element={<LandingPage/>}/>
